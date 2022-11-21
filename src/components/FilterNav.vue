e02e111ebfe65b83b40c39e7541712e6614747d8
Sheba Jose <shjose@bestpeers.com>
2022-11-21 11:41:53 +0530

    initial commit

---
+++ src/components/FilterNav.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav class="filter-nav">
+    <button @click="updatingFilter('all')"
+            :class="{ active : current === 'all'}"
+    >
+        View All
+    </button>
+    <button @click="updatingFilter('complete')"
+            :class="{ active : current === 'complete'}"
+    >
+        Complete
+    </button>
+    <button @click="updatingFilter('ongoing')"
+            :class="{ active : current === 'ongoing'}"
+    >
+        Ongoing
+    </button>
+  </nav>
+</template>
+
+<script>
+export default {
+    props: ['current'],
+methods:{
+  updatingFilter(by){
+    this.$emit('filterChange', by)
+  } 
+}
+}
+</script>
+
+<style>
+ .filter-nav button {
+    background: none;
+    border: none;
+    color: #bbb;
+    outline: none;
+    font-size: 12px;
+    text-transform: uppercase;
+    margin-right: 10px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    cursor: pointer;
+  }
+  .filter-nav button.active {
+    color: #555;
+  }
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AddProject from '../views/AddProject.vue'
+import EditProject from '../views/EditProject.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView
+  },
+  {
+    path: '/add',
+    name: 'AddProject',
+    component: AddProject
+  },
+  {
+    path: '/projects/:id',
+    name: 'EditProject',
+    component: EditProject,
+    props: true
+  }
+
+ 
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
